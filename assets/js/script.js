2e25d9e34c8c2ca05e4b67f0e1e20abe4da48c90
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2021-08-05 19:29:04 +0000

    Set the start button to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 alert("hello people");
+
+var playerOneChoices = new Object;
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    let start = document.getElementById("str_btn");
+    start.addEventListener("click",function(){
+        //Add the startGame function
+        console.log("hello");
+    })
+})

5d59798769d98c1cfd196fe7d86958aeb93b3d0b
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2021-08-08 07:17:43 +0000

    Add main orders to the start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 alert("hello people");
 
+var mainChoices = document.getElementById("main_choices_container");
+var mainGame = document.getElementById("main_game_container");
 var playerOneChoices = new Object;
 
 
@@ -12,3 +14,24 @@ document.addEventListener("DOMContentLoaded", function(){
         console.log("hello");
     })
 })
+
+
+
+
+
+
+
+
+
+function startGame(){
+    let startButton = document.getElementById("str_btn");
+
+    startButton.addEventListener("click", function(){
+        //make an check if the player has made all his choices
+
+        //Change the display propterty on choices and game contianer
+        mainChoices.style.display = "none";
+        mainGame.style.display = "flex";
+
+    })
+}

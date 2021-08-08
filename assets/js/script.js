428d8c166975cf73abce3c0ea220a7e11ee4fad2
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2021-08-08 14:13:11 +0000

    Set the reset button to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var startButton = document.getElementById("str_btn");
+var resetButton = document.getElementById("reset_btn");
 var mainChoices = document.getElementById("main_choices_container");
 var mainGame = document.getElementById("main_game_container");
 var playerOneChoices = new Object;
@@ -8,6 +9,7 @@ var playerOneChoices = new Object;
 document.addEventListener("DOMContentLoaded", function(){
 
     startGame();
+    resetGame();
 })
 
 
@@ -25,6 +27,18 @@ function startGame(){
         //Change the display propterty on choices and game contianer
         mainChoices.style.display = "none";
         mainGame.style.display = "flex";
+
+    })
+}
+
+function resetGame(){
+    resetButton.addEventListener("click", function(){
+        //make an check if the player has made all his choices
+
+        //Change the display propterty on choices and game contianer
+        mainGame.style.display = "none";
+        mainChoices.style.display = "flex";
+        
 
     })
 }
